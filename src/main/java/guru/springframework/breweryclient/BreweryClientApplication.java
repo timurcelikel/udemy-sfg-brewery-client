6806aacee139c36a966e548f2821146ca2b38f68
Timur Celikel <timur.celikel@proton.me>
2023-10-07 22:11:18 -0700

    Initial Commit

---
+++ src/main/java/guru/springframework/breweryclient/BreweryClientApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.breweryclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreweryClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreweryClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/breweryclient/BreweryClientApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.breweryclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreweryClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
