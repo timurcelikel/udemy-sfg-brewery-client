dd748058b4cb06858c7163c10f3b26087d831978
Timur Celikel <timur.celikel@proton.me>
2023-10-09 20:47:24 -0700

    Added additional RestTemplate CRUD operations

--- src/main/java/guru/springframework/breweryclient/web/client/BreweryRestTemplateClient.java
+++ src/main/java/guru/springframework/breweryclient/web/client/BreweryRestTemplateClient.java
@@ -5,12 +5,13 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import java.net.URI;
 import java.util.UUID;
 
 @Component
 public class BreweryRestTemplateClient {
 
-	public static final String BEER_PATH_V1 = "http://localhost:8080/api/v1/beer/";
+	public static final String BEER_PATH_V1 = "http://localhost:8080/api/v1/beer";
 	private final RestTemplate restTemplate;
 
 	public BreweryRestTemplateClient(RestTemplateBuilder restTemplateBuilder) {
@@ -18,6 +19,18 @@ public class BreweryRestTemplateClient {
 	}
 
 	public BeerDto getBeerById(UUID uuid) {
-		return restTemplate.getForObject(BEER_PATH_V1 + uuid.toString(), BeerDto.class);
+		return restTemplate.getForObject(BEER_PATH_V1 + "/" + uuid.toString(), BeerDto.class);
+	}
+
+	public URI saveNewBeer(BeerDto beerDto) {
+		return restTemplate.postForLocation(BEER_PATH_V1, beerDto);
+	}
+
+	public void updateBeer(UUID uuid, BeerDto beerDto) {
+		restTemplate.put(BEER_PATH_V1 + "/" + uuid, beerDto);
+	}
+
+	public void deleteBeer(UUID uuid) {
+		restTemplate.delete(BEER_PATH_V1 + "/" + uuid);
 	}
 }
--- src/test/java/guru/springframework/breweryclient/web/client/BreweryRestTemplateClientTest.java
+++ src/test/java/guru/springframework/breweryclient/web/client/BreweryRestTemplateClientTest.java
@@ -1,15 +1,19 @@
 package guru.springframework.breweryclient.web.client;
 
 import guru.springframework.breweryclient.web.model.BeerDto;
+import guru.springframework.breweryclient.web.model.BeerStyle;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.net.URI;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@Slf4j
 class BreweryRestTemplateClientTest {
 
 	@Autowired
@@ -19,5 +23,27 @@ class BreweryRestTemplateClientTest {
 	void getBeerById() {
 		BeerDto dto = breweryRestTemplateClient.getBeerById(UUID.randomUUID());
 		assertNotNull(dto);
+	}
+
+	@Test
+	void saveNewBeer() {
+		URI uri =
+				breweryRestTemplateClient.saveNewBeer(BeerDto.builder().beerName("California Honey").beerStyle(
+						BeerStyle.LAGER).upc(1234L).build());
+		assertNotNull(uri);
+	}
+
+	@Test
+	void updateBeer() {
+		breweryRestTemplateClient.updateBeer(UUID.randomUUID(),
+				BeerDto.builder().beerName("Chronic Ale").beerStyle(
+						BeerStyle.ALE).upc(1111L).build());
+	}
+
+	@Test
+	void deleteBeer() {
+		UUID uuid = UUID.randomUUID();
+		log.info("Delete UUID: " + uuid);
+		breweryRestTemplateClient.deleteBeer(uuid);
 	}
 }
